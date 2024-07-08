6edadf29ee378f98d08ce12b0aeb8724f1b9e3f5
IRF4N10 <irfankhanrirfan.official@gmail.com>
2024-07-08 21:16:27 +0600

    BubbleSort fixed

--- BubbleSort/src/BubbleSort.java
+++ BubbleSort/src/BubbleSort.java
@@ -6,10 +6,10 @@ public class BubbleSort {
     Scanner I = new Scanner(System.in);
 
     void IntBSort(){
-        System.out.printf("\nEnter the maximum size of the array\t");
+        System.out.printf("\nEnter the number of values: \t");
         int n = I.nextInt();
-        System.out.printf("\nEnter the elements of the array\t");
-        int array[] = new int[200];
+        System.out.printf("\nEnter the elements: \t");
+        int array[] = new int[n];
         for (int i = 0; i < n ; i++) {
             array[i] = I.nextInt();
         }
@@ -19,12 +19,15 @@ public class BubbleSort {
         }
         System.out.printf("\nImplementing the Bubble Sort:\n");
         int TEMP;
-        for(int i=0;i<n;i++){
-            for(int j=0; j<n;j++)
-                if(array[i]<array[j]){
+        //Iterate the whole array
+        for (int i = 0; i < n - 1; i++) {
+            //Iterate the array until the unsorted part
+            for(int j= 0; j<n-i-1;j++)
+                if (array[j] > array[j + 1]) {
+                    //swap
                     TEMP=array[j];
-                    array[j] = array[i];
-                    array[i] = TEMP;
+                    array[j] = array[j+1];
+                    array[j+1] = TEMP;
                 }
         }
         for(int i=0;i<n;i++){
@@ -45,12 +48,14 @@ public class BubbleSort {
         }
         System.out.printf("\nImplementing the Bubble Sort:\n");
         float TEMP;
-        for(int i=0;i<n;i++){
-            for(int j=0; j<n;j++)
-                if(array[i]<array[j]){
+        for (int i = 0; i < n - 1; i++) {
+            //Iterate the array until the unsorted part
+            for(int j= 0; j<n-i-1;j++)
+                if (array[j] > array[j + 1]) {
+                    //swap
                     TEMP=array[j];
-                    array[j] = array[i];
-                    array[i] = TEMP;
+                    array[j] = array[j+1];
+                    array[j+1] = TEMP;
                 }
         }
         for(int i=0;i<n;i++){
@@ -72,12 +77,14 @@ public class BubbleSort {
         }
         System.out.printf("\nImplementing the Bubble Sort:\n");
         char TEMP;
-        for(int i=0;i<n;i++){
-            for(int j=0; j<n;j++)
-                if(array[i]<array[j]){
+        for (int i = 0; i < n - 1; i++) {
+            //Iterate the array until the unsorted part
+            for(int j= 0; j<n-i-1;j++)
+                if (array[j] > array[j + 1]) {
+                    //swap
                     TEMP=array[j];
-                    array[j] = array[i];
-                    array[i] = TEMP;
+                    array[j] = array[j+1];
+                    array[j+1] = TEMP;
                 }
         }
         for(int i=0;i<n;i++){
