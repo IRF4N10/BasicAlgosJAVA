57f25eeb7da50f3b7515556f1990af2df00b9efb
IRF4N10 <irfankhanrirfan.official@gmail.com>
2024-07-14 15:43:49 +0600

    problem solved

--- InsertionSort/InsertionSort/src/Main.java
+++ InsertionSort/InsertionSort/src/Main.java
@@ -5,39 +5,40 @@ import static java.lang.System.exit;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner I = new Scanner(System.in);
-	InsertionSort ins_sort = new InsertionSort();
-	System.out.printf("Enter the type of input data:\n");
-        loop1:
-        while (true) {
-            System.out.printf("\n\t***Select the Data Type***\nPress 1 to select Integer \nPress 2 to select Float\n" +
-                        "Press 3 to select Character\n\nPress 0 to Exit\n");
+	try (Scanner In = new Scanner(System.in)) {
+        InsertionSort ins_sort = new InsertionSort();
+        System.out.printf("Enter the type of input data:\n");
+            
+            while (true) {
+                System.out.printf("\n\t***Select the Data Type***\nPress 1 to select Integer \nPress 2 to select Float\n" +
+                            "Press 3 to select Character\n\nPress 0 to Exit\n");
 
-            int op = I.nextInt();
-            switch (op) {
-                case 0: {
-                    exit(0);
-                }
-                case 1: {
-                    System.out.printf("\nInteger Data Type is selected\n");
-                    ins_sort.IntISort();
-                    break;
-                }
-                case 2: {
-                    System.out.printf("\nFloat Data Type is selected\n");
-                    ins_sort.FltISort();
-                    break;
-                }
-                case 3: {
-                    System.out.printf("\nCharacter Data Type is selected\n");
-                    ins_sort.CharISort();
-                    break;
-                }
-                default: {
-                    System.out.printf("\tError!!!\nInvalid Operation selected");
-                    break ;
+                int op = In.nextInt();
+                switch (op) {
+                    case 0: {
+                        exit(0);
+                    }
+                    case 1: {
+                        System.out.printf("\nInteger Data Type is selected\n");
+                        ins_sort.IntISort();
+                        break;
+                    }
+                    case 2: {
+                        System.out.printf("\nFloat Data Type is selected\n");
+                        ins_sort.FltISort();
+                        break;
+                    }
+                    case 3: {
+                        System.out.printf("\nCharacter Data Type is selected\n");
+                        ins_sort.CharISort();
+                        break;
+                    }
+                    default: {
+                        System.out.printf("\tError!!!\nInvalid Operation selected");
+                        break ;
+                    }
                 }
             }
-        }
+    }
     }
 }
